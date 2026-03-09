70a318be5789dcf088fffb3baa83b56b5ad43a7e
Jean Didas <22td465@esisalama.org>
2026-03-09 17:21:35 +0100

    fix(home): provide description variable for home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,19 +4,11 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
-   // Dans HomeController
-public function index()
-{
-    // Données statiques pour tester
-    $services = [
-        ['title' => 'Construction', 'description' => '...'],
-        ['title' => 'Rénovation', 'description' => '...'],
-    ];
-    
-    return view('home.index', [
-        'title' => 'MLS Construction',
-        'services' => $services,
-        'realisations' => [] // Vide
-    ]);
-}
+    public function index()
+    {
+        return view('home.index', [
+            'title' => 'MLS Construction',
+            'description' => 'Site officiel MLS Construction',
+        ]);
+    }
 }
